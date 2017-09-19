fc827dbb39af70c7490ba814d5d4d3757ea1ea53
knaugle03 <midget90>
2017-09-19 17:18:27 -0300

    working through some issues

--- Labs/Lab1/scripts.js
+++ Labs/Lab1/scripts.js
@@ -9,19 +9,14 @@ function pushToFront(x){
     }
 }
 function toggleClass(){
-    //alert(x.className);
     var d1 = document.getElementById("div1");
     var d2 = document.getElementById("div2");
     if(d1.className == "front"){
         d1.className = "back";
         d2.className = "front";
-        d1.style.opacity = 0.5;
-        d2.style.opacity = 1;
     }
-    else{
+    else if(d2.className == "front"){
         d1.className = "front";
         d2.className = "back";
-        d1.style.opacity = 1;
-        d2.style.opacity = .5;
     }
 }
