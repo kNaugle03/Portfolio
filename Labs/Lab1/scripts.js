f9faeff1e0b5811dd61dd7151e2ab5ec75c52946
knaugle03 <midget90>
2017-09-25 19:50:24 -0300

    Part 2 Completed

--- Labs/Lab1/scripts.js
+++ Labs/Lab1/scripts.js
@@ -1,22 +1,48 @@
-function pushToBack(x){
-    if(x.className = "front"){
-        x.className = "back";
-    }
-}
-function pushToFront(x){
-    if(x.className = "back"){
-        x.className = "front";
-    }
-}
+// Two failed attempts at doing part 1
+
+// function pushToBack(x){
+//     if(x.className = "front"){
+//         x.className = "back";
+//     }
+// }
+//
+// function pushToFront(x){
+//     if(x.className = "back"){
+//         x.className = "front";
+//     }
+// }
+
+document.getElementById("div1").addEventListener("dblclick", toggleClass);
+document.getElementById("div2").addEventListener("dblclick", toggleClass);
+
 function toggleClass(){
     var d1 = document.getElementById("div1");
     var d2 = document.getElementById("div2");
-    if(d1.className == "front"){
+    if(d1.className == "front") {
         d1.className = "back";
         d2.className = "front";
     }
     else if(d2.className == "front"){
         d1.className = "front";
         d2.className = "back";
+    }
+}
+
+// got this cool idea from this codepen - https://codepen.io/sureshrkm/pen/ZbzBpr
+// I am borrowing heavily from this idea but I've coded it by hand and used my own naming conventions
+var slides = document.getElementsByClassName("slide");
+
+var accHeaders = document.getElementsByClassName("accordion-heading");
+for(var i = 0; i<accHeaders.length;i++){
+    accHeaders[i].addEventListener("click", toggleAccordion);
+}
+
+function toggleAccordion(){
+    var target = this.parentNode.className;
+    for(var i = 0;i<slides.length;i++){
+        slides[i].className = "slide closed";
+    }
+    if(target == "slide closed"){
+        this.parentNode.className = "slide open";
     }
 }
