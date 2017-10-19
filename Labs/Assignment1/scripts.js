86bd6971c4f69b74590495dfd2618dc50d5c7978
knaugle03 <midget90>
2017-10-19 20:03:46 -0300

    Initial commit. Have table being drawn and Shapes being populated

--- Labs/Assignment1/scripts.js
+++ Labs/Assignment1/scripts.js
@@ -8,13 +8,21 @@
                          [0,0,0,0,0,1],
                          [0,1,0,2,0,1],
                          [0,0,0,2,0,0]];
-    var correctArray = [[],[],[],[],[],[]];
+    var p = "purple";
+    var g = "green";
+    var correctArray = [[g,g,p,p,g,p],
+                        [p,p,g,g,p,g],
+                        [p,p,g,p,g,g],
+                        [g,g,p,p,g,p],
+                        [g,p,g,g,p,p],
+                        [p,g,p,g,p,g]];
 
     // creating the 2D array and populating them with Shape objects
     for(var row=0;row<colorArray.length;row++){
         colorArray[row] = new Array(6);
         for(var col=0;col<colorArray[row].length;col++){
-            colorArray[row][col] = new Square(defaultColors[row][col]);
+            colorArray[row][col] = new Square(defaultColors[row][col], correctArray[row][col]);
+
         }
     }
 
@@ -34,30 +42,94 @@
             t.appendChild(tr);
             for(var y=0;y<colorArray.length;y++){
                 var r = document.getElementById("row" + x);
-                var node = colorArray[x][y].print("col"+x+"-"+y);
+                var node = colorArray[x][y].print("cell"+x+"-"+y);
                 r.appendChild(node);
             }
         }
     }
 
-    //define my square object
-    function Square(c){
-        this.colors = ["lightgray", "purple", "green"];
-        this.currentColor = this.colors[c];
-        this.index = 0;
+    // function to cycle through the colors
+    function changeColor(){
+        var colors = ["lightgray", "purple", "green"];
+        var current = this.style.backgroundColor;
 
-        this.changeColor = function(){
-            this.index++;
-            if(this.index >= this.colors.length){
-                this.index = 0;
+        if(current === colors[0]){
+            current = colors[1];
+            this.style.backgroundColor = current;
+        }
+        else if(current === colors[1]){
+            current = colors[2];
+            this.style.backgroundColor = current;
+        }
+        else{
+            current = colors[0];
+            this.style.backgroundColor = current;
+        }
+
+    }
+
+    // function to check the correctness of the puzzle
+    function checkPuzzle() {
+
+    }
+
+    function showIncorrect() {
+        if(this.checked) {
+            for (var x = 0; x < colorArray.length; x++) {
+                for (var y = 0; y < colorArray.length; y++) {
+                    if ((colorArray[x][y].currentColor !== colorArray[x][y].correctColor)
+                        && colorArray[x][y].currentColor !== "lightgray") {
+                        colorArray[x][y].innerHTML.style.visibility = "visible";
+                    }
+                }
             }
-            this.currentColor = this.colors[this.index];
-        };
+        }
+        else if(!this.checked){
+            for (var a = 0; a< colorArray.length; a++) {
+                for (var b = 0; b < colorArray.length; b++) {
+                    colorArray[a][b].innerHTML.style.visibility = "hidden";
+                }
+            }
+        }
+    }
+
+    // set all my listeners
+    function setListeners() {
+        var clickables = document.getElementsByClassName("clickable");
+        for(var i=0;i<clickables.length;i++){
+            clickables[i].addEventListener("click", changeColor);
+        }
+        document.getElementById("check-puzzle").addEventListener("click", checkPuzzle);
+        document.getElementById("show-incorrect").addEventListener("click", showIncorrect)
+        // THIS IS THE OLD WAY
+        // for(var x=0;x<colorArray.length;x++){
+        //     for(var y=0;y<colorArray.length;y++){
+        //         var id = "cell"+x+"-"+y;
+        //         var tag = document.getElementById(id);
+        //         if(tag.style.backgroundColor === "lightgray") {
+        //             tag.addEventListener("click", changeColor)
+        //         }
+        //     }
+        // }
+    }
+
+    //define my square object
+    function Square(current, correct){
+        this.colors = ["lightgray", "purple", "green"];
+        this.currentColor = this.colors[current];
+        this.correctColor = correct;
+        this.incorrect = document.createElement("SPAN");
+        this.incorrect.className = "incorrect";
+        this.incorrect.innerHTML = "x";
 
         this.print = function (id) {
             var node = document.createElement("TD");
             node.id = id;
             node.style.backgroundColor = this.currentColor;
+            if(node.style.backgroundColor === "lightgray"){
+                node.className = "clickable";
+                node.appendChild(this.incorrect);
+            }
             return node;
         };
 
@@ -66,5 +138,6 @@
 
 
     drawTable(); // DRAW THE TABLE
+    setListeners(); // ADD MY EVENT LISTENERS
 
 })(); // IIFE End
